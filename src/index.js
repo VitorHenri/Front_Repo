722efd0c312ca3ff489ce9f17befb4fc4338e909
VitorHenri <vitorsantos.info@gmail.com>
2022-08-28 20:48:28 -0300

    First Commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import {createRoot} from 'react-dom/client';
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+
+root.render(
+    <div className="principal">
+        <strong>Olá React</strong>
+    </div>
+
+)
